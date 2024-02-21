49ad3ecdca3badc0770034fa9f995b99a96e40ef
savva <savva.lambin@gmail.com>
2024-02-21 15:03:16 +0000

    Add: event-bus, query services

---
+++ event-bus/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const axios = require('axios');
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.post('/events', async (req, res) => {
+  const event = req.body;
+
+  try {
+    await axios.post('http://localhost:4000/events', event);
+    await axios.post('http://localhost:4001/events', event);
+    await axios.post('http://localhost:4002/events', event);
+    res.send({ status: 'OK' });
+  } catch (error) {
+    console.log(error.message);
+  }
+});
+
+app.listen(4005, () => {
+  console.log('Listening on 4005');
+});
