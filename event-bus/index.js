67a964393a50328278b7d90a629f5f30598140e2
savva <savva.lambin@gmail.com>
2024-02-24 16:01:06 +0000

    Add: get endpoint to event bus, fetch events on query service wake up

--- event-bus/index.js
+++ event-bus/index.js
@@ -5,18 +5,27 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+const events = [];
+
 app.post('/events', async (req, res) => {
   const event = req.body;
+
+  events.push(event);
 
   try {
     await axios.post('http://localhost:4000/events', event);
     await axios.post('http://localhost:4001/events', event);
-    await axios.post('http://localhost:4002/events', event);
     await axios.post('http://localhost:4003/events', event);
+    await axios.post('http://localhost:4002/events', event);
+
     res.send({ status: 'OK' });
   } catch (error) {
     console.log(error.message);
   }
+});
+
+app.get('/events', (req, res) => {
+  res.send(events);
 });
 
 app.listen(4005, () => {
